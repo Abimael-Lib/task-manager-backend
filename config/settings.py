8f94bd5c37125af540b535adb612a436555506af
Abimael-Lib <abimaelmaidana007@gmail.com>
2025-07-08 16:52:58 -0300

    Modelo de Tarea creado, relacionado con Usuario, migraciones y admin configurado

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # apps internas
+    'tareas',
+
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps externas
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ tareas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tarea(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+
