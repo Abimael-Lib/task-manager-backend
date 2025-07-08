1fc02fb9f266c90edeca0927a1566eb01aba14af
Abimael-Lib <abimaelmaidana007@gmail.com>
2025-07-08 17:35:13 -0300

    API RESTful creada para CRUD de tareas (serializers, views, urls)

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Tarea
+from .serializer import TareaSerializer
 
-# Create your views here.
+
+class TareaViewSet(viewsets.ModelViewSet):
+    serializer_class = TareaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Tarea.objects.filter(usuario=self.request.user).order_by('-fecha_creacion')
+
+    def perform_create(self, serializer):
+        serializer.save(usuario.self.request.user)
+
